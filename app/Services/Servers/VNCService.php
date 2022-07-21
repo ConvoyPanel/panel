14b0c41e1a27ed89258fa5dedf72b76575dcb0d5
Eric Wang <eric.wang401@gmail.com>
2022-07-21 13:18:55 -0500

    get temp vnc token

--- app/Services/Servers/VNCService.php
+++ app/Services/Servers/VNCService.php
@@ -3,6 +3,8 @@
 namespace App\Services\Servers;
 
 use App\Services\ProxmoxService;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 
 class VNCService extends ProxmoxService
 {
@@ -10,9 +12,7 @@ class VNCService extends ProxmoxService
     {
         $vnc_role = [
             "roleid" => "VNC",
-            "privs" => [
-                "VM.Console"
-            ]
+            "privs" => "VM.Console"
         ];
         return $this->proxmox()->access()->roles()->post($vnc_role);
     }
@@ -37,5 +37,38 @@ class VNCService extends ProxmoxService
     {
 
         return $this->instance()->getPve()->getApiURL() . $this->instance()->vncwebsocket()->getWebsocketEndpoint();
+    }
+
+    public function temp()
+    {
+        $expirationDate = Carbon::now();
+        $expirationDate->addDay();
+
+        $user = [
+            "enable" => '1',
+            // IF YOU EDIT THIS BELOW. WATCH OUT! PROXMOX HAS A USERID LENGTH LIMIT. TEST BEFORE YOU COMMIT!!!
+            "userid" => 'convoy-' . Str::random(50) . '@pam',
+            // below fields doesn't work because it tries to change the password of an existing user
+            //"email" => "",
+            //"password" => Str::random(12),
+            'expire' => $expirationDate->timestamp,
+        ];
+
+        $role = [
+            "roleid" => "convoy-vnc",
+            "privs" => "VM.Console"
+        ];
+
+        $this->mainInstance()->access()->users()->post($user);
+
+        $this->mainInstance()->access()->roles()->post($role);
+
+        $this->mainInstance()->access()->acl()->put([
+            'path' => '/vms/' . '100',
+            'users' => $user['userid'],
+            'roles' => $role['roleid']
+        ]);
+
+        return $this->mainInstance()->access()->users()->userId($user['userid'])->token()->tokenId('convoy-vnc')->post(['userid' => $user['userid']]);
     }
 }
