d8f90a97bc40433868bed841e4ccf67de5db6927
Eric Wang <eric.wang401@gmail.com>
2022-08-01 14:25:02 +0000

    Add rest of the API routes

---
+++ app/Http/Requests/Application/Nodes/Addresses/UpdateAddressRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Application\Nodes\Addresses;
+
+use App\Enums\Network\AddressType;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+
+class UpdateAddressRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array{
+     *     server_id: int | null,
+     *     node_id: int,
+     *     address: string,
+     *     cidr: int,
+     *     gateway: string,
+     *     type: string
+     */
+    public function rules()
+    {
+        return [
+            'server_id' => 'exists:servers,id|nullable',
+            'node_id' => 'required|exists:nodes,id',
+            'address' => 'ip',
+            'cidr' => 'numeric|required',
+            'gateway' => 'ip',
+            'type' => [new Enum(AddressType::class), 'required'],
+        ];
+    }
+}
