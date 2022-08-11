1a765e267837b1d6e2516f2d3c79e3803416dd92
Kjartan Hrafnkelsson <kjartan@kjartan.io>
2022-08-11 03:42:33 +0000

    Update and rename lang/en/auth.php to lang/is/auth.php

--- lang/is/auth.php
+++ lang/is/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Innskráning tókst ekki.',
+    'password' => 'Lykilorðið sem þú slóst inn er rangt.',
+    'throttle' => 'Of margar tilraunir. Reyndur aftur eftir :seconds sekúndur.',
 
 ];
