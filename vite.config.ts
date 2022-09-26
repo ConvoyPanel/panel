a37772879e5484db9bdf2991a649e438dad24115
Eric Wang <eric.wang401@gmail.com>
2022-09-26 23:02:04 +0000

    Remove lodash

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import react from '@vitejs/plugin-react'
 import macrosPlugin from 'vite-plugin-babel-macros'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [react(), laravel(['resources/js/app.tsx']), macrosPlugin()],
+  plugins: [react(), laravel(['resources/js/app.tsx']), macrosPlugin(), /* visualizer() */],
   build: {
     target: ['es2020'],
   },
