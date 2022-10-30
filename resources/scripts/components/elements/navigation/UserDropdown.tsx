6db72e63b8398e683e7b6d4a500187961d2783dc
Eric Wang <eric.wang401@gmail.com>
2022-10-30 04:24:09 +0000

    Add signing out

--- resources/scripts/components/elements/navigation/UserDropdown.tsx
+++ resources/scripts/components/elements/navigation/UserDropdown.tsx
@@ -1,23 +1,35 @@
-import { Avatar, Menu } from '@mantine/core'
+import { Avatar, LoadingOverlay, Menu } from '@mantine/core'
 import { useStoreState } from '@/state'
 import { getInitials } from '@/util/helpers'
+import { useState } from 'react'
+import http from '@/api/http'
 
 const UserDropdown = () => {
   const user = useStoreState((state) => state.user.data!)
+  const [isLoggingOut, setIsLoggingOut] = useState(false)
+
+  const logout = () => {
+    setIsLoggingOut(true)
+    http.post('/logout').finally(() => {
+      window.location.replace('/')
+    })
+  }
 
   return (
-    <Menu width={175} shadow='lg'>
-      <Menu.Target>
-        <button className='flex items-center space-x-3 bg-transparent ring-transparent rounded-sm ring-4 hover:ring-stone-100 dark:hover:ring-stone-900 hover:bg-stone-100 dark:hover:bg-stone-900 transition'>
-          <p className='text-auto font-medium text-sm'>{user.name}</p>
-          <Avatar color='blue' size='md' radius='xl'>
-            {getInitials(user.name)}
-          </Avatar>
-        </button>
-      </Menu.Target>
+    <>
+      <LoadingOverlay visible={isLoggingOut} />
+      <Menu width={175} shadow='lg'>
+        <Menu.Target>
+          <button className='flex items-center space-x-3 bg-transparent ring-transparent rounded-sm ring-4 hover:ring-stone-100 dark:hover:ring-stone-900 hover:bg-stone-100 dark:hover:bg-stone-900 transition'>
+            <p className='text-auto font-medium text-sm'>{user.name}</p>
+            <Avatar color='blue' size='md' radius='xl'>
+              {getInitials(user.name)}
+            </Avatar>
+          </button>
+        </Menu.Target>
 
-      <Menu.Dropdown>
-        {/* {auth.user.root_admin && route().current('admin.*') ? (
+        <Menu.Dropdown>
+          {/* {auth.user.root_admin && route().current('admin.*') ? (
         <Menu.Item
           onClick={() => Inertia.visit(route('dashboard'))}
         >
@@ -38,9 +50,10 @@ const UserDropdown = () => {
         ''
       )}{' '}
         <Menu.Divider /> */}
-        <Menu.Item color='red'>Log Out</Menu.Item>
-      </Menu.Dropdown>
-    </Menu>
+          <Menu.Item color='red' onClick={logout}>Log Out</Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+    </>
   )
 }
 
