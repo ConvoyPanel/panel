a4fd0eff4ef66f11cf79e96da0f586f1a85342d1
Eric Wang <eric.wang401@gmail.com>
2022-11-05 03:02:15 +0000

    Add favicon logo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <link rel="icon" href="favicon.svg" sizes="any" type="image/svg+xml">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700&display=swap">
