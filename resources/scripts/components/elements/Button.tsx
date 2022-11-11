09701b1d34908a19c07be432d1d8845f2f2cbf3a
Eric Wang <eric.wang401@gmail.com>
2022-11-11 02:25:48 +0000

    Update button

--- resources/scripts/components/elements/Button.tsx
+++ resources/scripts/components/elements/Button.tsx
@@ -3,7 +3,7 @@ import tw from 'twin.macro'
 
 interface Props {
   variant?: 'outline' | 'filled'
-
+  color?: 'primary' | 'danger' | 'accent'
   size?: 'sm'
 }
 
@@ -12,7 +12,7 @@ const Button = styled.button<Props>`
 
   ${({ size }) => (size === 'sm' ? tw`px-3 py-1.5` : tw`px-5 h-9`)}
 
-    ${({ variant = 'outline' }) => variant === 'outline' ? tw`border-accent-200 hover:border-foreground active:border-foreground text-accent-500 hover:text-foreground active:text-foreground active:bg-accent-200 bg-background` : ''}
+  ${({ variant = 'outline', color = 'accent' }) => variant === 'outline' ? tw`border-accent-200 hover:border-foreground active:border-foreground text-accent-500 hover:text-foreground active:text-foreground active:bg-accent-200 bg-background` : ''}
 `
 
 export default Button
