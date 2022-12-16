d0f0242730c2717fbed92a782229d033deacffbf
Eric Wang <eric.wang401@gmail.com>
2022-12-16 05:52:49 +0000

    Fix positioning

--- resources/scripts/components/elements/Menu.tsx
+++ resources/scripts/components/elements/Menu.tsx
@@ -3,83 +3,91 @@ import { ComponentProps, forwardRef, Fragment, ReactElement, ReactNode } from 'r
 import { Float, FloatProps } from '@headlessui-float/react'
 
 interface ButtonProps {
-  children?: ReactNode
+    children?: ReactNode
 }
 
 interface ItemsProps {
-  children: ReactNode
-  marginTop?: number | string
+    children: ReactNode
+    marginTop?: number | string
 }
 
 interface Menu extends React.FC<MenuProps> {
-  Button: React.FC<ButtonProps>
-  Items: React.FC<ItemsProps>
-  Item: React.FC<ItemProps>
-  Divider: React.FC
+    Button: React.FC<ButtonProps>
+    Items: React.FC<ItemsProps>
+    Item: React.FC<ItemProps>
+    Divider: React.FC
 }
 
 interface MenuProps {
-  children: ReactElement[]
-  className?: string
+    children: ReactElement[]
+    className?: string
 }
 
 interface ItemProps extends Omit<ComponentProps<'button'>, 'className'> {
-  color?: 'accent' | 'danger'
+    color?: 'accent' | 'danger'
 }
 
 const Menu: Menu = ({ children, className }) => {
-  return (
-
-    <HMenu>
-    <Float
-
-flip
-portal
-      enter='transition ease-out duration-100'
-      enterFrom='opacity-0 -translate-y-[1rem]'
-      enterTo='opacity-100 ranslate-y-0'
-      leave='ease-in duration-75'
-      leaveFrom='opacity-100 translate-y-0'
-      leaveTo='opacity-0 -translate-y-[1rem]'
-      tailwindcssOriginClass
-    >
-      {children}
-    </Float>
-    </HMenu>
-  )
+    return (
+        <HMenu>
+            <Float
+                placement='bottom-end'
+                flip
+                portal
+                enter='transition ease-out duration-100'
+                enterFrom='opacity-0 -translate-y-[1rem]'
+                enterTo='opacity-100 ranslate-y-0'
+                leave='ease-in duration-75'
+                leaveFrom='opacity-100 translate-y-0'
+                leaveTo='opacity-0 -translate-y-[1rem]'
+                tailwindcssOriginClass
+            >
+                {children}
+            </Float>
+        </HMenu>
+    )
 }
 
 Menu.Button = forwardRef<HTMLDivElement, ButtonProps>(({ children }, ref) => {
-  return <HMenu.Button ref={ref} as='div'>{children}</HMenu.Button>
+    return (
+        <HMenu.Button ref={ref} as='div'>
+            {children}
+        </HMenu.Button>
+    )
 })
 
 Menu.Items = forwardRef<HTMLDivElement, ItemsProps>(({ children, marginTop }, ref) => {
-  return (
-      <HMenu.Items ref={ref} className='right-0 flex flex-col absolute w-56 p-2 overflow-auto rounded bg-background shadow-lg dark:shadow-none border border-accent-200 focus:outline-none sm:text-sm z-[3000]'>
-        {children}
-      </HMenu.Items>
-  )
+    return (
+        <HMenu.Items
+            ref={ref}
+            className='right-0 flex flex-col absolute w-56 p-2 overflow-auto rounded bg-background shadow-lg dark:shadow-none border border-accent-200 focus:outline-none sm:text-sm z-[3000]'
+        >
+            {children}
+        </HMenu.Items>
+    )
 })
 
 Menu.Item = ({ children, color, disabled, ...props }) => {
-  return (
-    <HMenu.Item
-      as='button'
-      className={`w-full text-left px-2 h-12 sm:h-9 bg-transparent ${
-        disabled ? 'text-accent-300 cursor-not-allowed' : color === 'danger'
-        ? 'text-error sm:hover:bg-error-lighter active:bg-error-lighter sm:active:bg-error-lighter'
-        : 'text-accent-500 sm:hover:bg-accent-200 sm:active:bg-accent-200 active:bg-accent-200'
-      } rounded`}
-      disabled={disabled}
-      {...props}
-    >
-      {children}
-    </HMenu.Item>
-  )
+    return (
+        <HMenu.Item
+            as='button'
+            className={`w-full text-left px-2 h-12 sm:h-9 bg-transparent ${
+                disabled
+                    ? 'text-accent-300 cursor-not-allowed'
+                    : color === 'danger'
+                    ? 'text-error sm:hover:bg-error-lighter active:bg-error-lighter sm:active:bg-error-lighter'
+                    : 'text-accent-500 sm:hover:bg-accent-200 sm:active:bg-accent-200 active:bg-accent-200'
+            } rounded`}
+            disabled={disabled}
+            {...props}
+        >
+            {children}
+        </HMenu.Item>
+    )
 }
 
 Menu.Divider = () => {
-  return <div className='w-full my-1 h-[1px] bg-accent-200' />
+    return <div className='w-full my-1 h-[1px] bg-accent-200' />
 }
 
 export default Menu
