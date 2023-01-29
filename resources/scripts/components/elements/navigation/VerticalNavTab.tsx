80fdfbcf03b9ab5dd82adc19eb3035409e2ac455
Eric Wang <eric.wang401@gmail.com>
2023-01-29 19:28:47 +0000

    unround vertical nav tab on mobile

--- resources/scripts/components/elements/navigation/VerticalNavTab.tsx
+++ resources/scripts/components/elements/navigation/VerticalNavTab.tsx
@@ -4,10 +4,10 @@ import { NavLink, NavLinkProps } from 'react-router-dom'
 import tw from 'twin.macro'
 
 const VerticalNavTab = styled(NavLink)`
-${tw`transition text-sm block rounded text-accent-600`}
+${tw`transition text-sm block text-accent-600`}
 
 @media (min-width: 960px) {
-    ${tw`px-3 py-2 hover:bg-accent-200 hover:text-foreground`}
+    ${tw`px-3 py-2 hover:bg-accent-200 hover:text-foreground rounded`}
 
     &.active {
         ${tw`text-foreground font-medium`}
