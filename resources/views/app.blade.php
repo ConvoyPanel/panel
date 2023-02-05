72e2a0e2731294f35fb586fa16f82e0c061704c3
Eric Wang <eric.wang401@gmail.com>
2023-02-05 03:57:37 +0000

    add google analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,6 +27,20 @@
         <!-- Scripts -->
         @viteReactRefresh
         @vite('resources/scripts/main.tsx')
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-F0PFRTBLR6"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-F0PFRTBLR6');
+
+            gtag('event', 'meta', {
+                'version': '{{ config('app.version') }}'
+            });
+        </script>
     </head>
     <body class="font-sans antialiased">
         <div id="root"></div>
