c60acd68904fe7a75a4e837efee53f0e8d3d0c5b
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-04-14 22:17:29 -0500

    New translations index.php (Turkish)

---
+++ lang/tr/index.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'show_all_servers' => 'Show all servers',
+    'no_servers_admin_empty_state' => 'There are no servers.',
+    'no_servers_empty_state' => 'You have no servers.'
+];
