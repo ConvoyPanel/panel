e97c6ac32a687f57b912a60e0bbfaf7253f12e27
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-04-14 22:16:34 -0500

    New translations users.php (Japanese)

---
+++ lang/ja/users.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'new_user' => 'New User',
+    'create_modal' => [
+        'title' => 'Create a User',
+        'is_admin_warning' => 'You are giving this user administrative privileges.'
+    ],
+    'user_info' => [
+        'title' => 'User Information',
+        'override_pass' => 'Override password'
+    ],
+    'delete_user' => [
+        'title' => 'Delete User',
+        'description' => 'The user and its associated data will be deleted.',
+        'has_servers_error' => 'This user cannot be deleted with servers still associated with it.'
+    ]
+];
