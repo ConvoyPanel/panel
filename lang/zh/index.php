646286d0445dd03d8e2374b281cd9b216cde16a7
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-04-17 20:54:47 -0500

    New translations index.php (Chinese Simplified)

--- lang/zh/index.php
+++ lang/zh/index.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'show_all_servers' => 'Show all servers',
-    'no_servers_admin_empty_state' => 'There are no servers.',
-    'no_servers_empty_state' => 'You have no servers.'
+    'show_all_servers' => '显示所有服务器',
+    'no_servers_admin_empty_state' => '这里还没有服务器可供管理。',
+    'no_servers_empty_state' => '你尚未拥有任何服务器。'
 ];
