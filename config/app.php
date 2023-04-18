7c0c568fa903d085ff3d9f164cf408b7a9c09582
Convoy CI <ci@convoypanel.com>
2023-04-18 22:50:42 +0000

    bump version for release

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => 'canary',
+    'version' => '3.9.0-rc.8',
 
     /*
     |--------------------------------------------------------------------------
