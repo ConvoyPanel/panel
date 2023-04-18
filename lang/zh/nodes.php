cae1945fa6db286fc7c82a52c34898f925f09c40
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-04-17 20:54:43 -0500

    New translations nodes.php (Chinese Simplified)

--- lang/zh/nodes.php
+++ lang/zh/nodes.php
@@ -35,6 +35,6 @@ return [
     'delete_node' => [
         'title' => '删除节点',
         'description' => '节点将从 Convoy 中永久删除。此操作不能被复原及撤销。',
-        'has_servers_error' => 'You cannot delete a node that has servers assigned to it.',
+        'has_servers_error' => '你无法删除一个分配有服务器的节点。',
     ]
 ];
