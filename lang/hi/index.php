7e0d39a2ad45f19e2144221597bcb62f0f98690f
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-04-17 23:31:39 -0500

    New translations index.php (Hindi)

---
+++ lang/hi/index.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'show_all_servers' => 'सभी सर्वर प्रदर्शित करें',
+    'no_servers_admin_empty_state' => 'कोई उपलब्ध सर्वर नहीं',
+    'no_servers_empty_state' => 'You have no servers.'
+];
