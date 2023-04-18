04f60e059f2d84f93f75559991883c9acf0d6fcf
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-04-18 08:31:34 -0500

    New translations index.php (Spanish)

--- lang/es/index.php
+++ lang/es/index.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'show_all_servers' => 'Show all servers',
-    'no_servers_admin_empty_state' => 'There are no servers.',
-    'no_servers_empty_state' => 'You have no servers.'
+    'show_all_servers' => 'Mostrar todos los servidores',
+    'no_servers_admin_empty_state' => 'No hay servidores.',
+    'no_servers_empty_state' => 'No tienes servidores.'
 ];
