bf20c68e692dd96f8988a37ae93bf121a17cb4b6
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-04-28 21:49:34 -0500

    New translations index.php (Dutch)

--- lang/nl/index.php
+++ lang/nl/index.php
@@ -3,5 +3,5 @@
 return [
     'show_all_servers' => 'Show all servers',
     'no_servers_admin_empty_state' => 'There are no servers.',
-    'no_servers_empty_state' => 'You have no servers.'
+    'no_servers_empty_state' => 'You have no servers.',
 ];
