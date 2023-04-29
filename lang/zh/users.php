4611840a0648e69ecc925bee1aa9e6f41c142d3d
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-04-28 21:49:23 -0500

    New translations users.php (Chinese Traditional)

--- lang/zh/users.php
+++ lang/zh/users.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-    'new_user' => '新建用户',
+    'new_user' => 'New User',
     'create_modal' => [
-        'title' => '创建用户',
-        'is_admin_warning' => '你正给予用户管理员权限。',
+        'title' => 'Create a User',
+        'is_admin_warning' => 'You are giving this user administrative privileges.',
     ],
     'user_info' => [
-        'title' => '用户信息',
-        'override_pass' => '覆盖密码',
+        'title' => 'User Information',
+        'override_pass' => 'Override password',
     ],
     'delete_user' => [
-        'title' => '删除用户',
-        'description' => '用户及其关联数据将被删除。',
-        'has_servers_error' => '由于还有服务器与该用户关联，你目前不能删除该用户。',
+        'title' => 'Delete User',
+        'description' => 'The user and its associated data will be deleted.',
+        'has_servers_error' => 'This user cannot be deleted with servers still associated with it.',
     ],
 ];
