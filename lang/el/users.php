d1e8cdd25ae55d3e34ca2e9db2d1ed9a9fd1bac0
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-04-28 21:48:57 -0500

    New translations users.php (Greek)

--- lang/el/users.php
+++ lang/el/users.php
@@ -4,15 +4,15 @@ return [
     'new_user' => 'New User',
     'create_modal' => [
         'title' => 'Create a User',
-        'is_admin_warning' => 'You are giving this user administrative privileges.'
+        'is_admin_warning' => 'You are giving this user administrative privileges.',
     ],
     'user_info' => [
         'title' => 'User Information',
-        'override_pass' => 'Override password'
+        'override_pass' => 'Override password',
     ],
     'delete_user' => [
         'title' => 'Delete User',
         'description' => 'The user and its associated data will be deleted.',
-        'has_servers_error' => 'This user cannot be deleted with servers still associated with it.'
-    ]
+        'has_servers_error' => 'This user cannot be deleted with servers still associated with it.',
+    ],
 ];
