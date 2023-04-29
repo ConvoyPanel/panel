4f02841547b9b47cef95cfb09089796bd90e1cae
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-04-28 21:52:25 -0500

    New translations index.php (Hindi)

--- lang/hi/index.php
+++ lang/hi/index.php
@@ -3,5 +3,5 @@
 return [
     'show_all_servers' => 'सभी सर्वर प्रदर्शित करें',
     'no_servers_admin_empty_state' => 'कोई उपलब्ध सर्वर नहीं',
-    'no_servers_empty_state' => 'आपको कोई सर्वर असाइन नहीं किया गया है'
+    'no_servers_empty_state' => 'आपको कोई सर्वर असाइन नहीं किया गया है',
 ];
