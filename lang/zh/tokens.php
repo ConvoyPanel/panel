aec354c1734749dd2b95ee1f05b74f55ca41cac1
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-04-28 21:48:51 -0500

    New translations tokens.php (Chinese Simplified)

--- lang/zh/tokens.php
+++ lang/zh/tokens.php
@@ -2,11 +2,11 @@
 
 return [
     'create_modal' => [
-        'title' => '创建令牌'
+        'title' => '创建令牌',
     ],
     'created_modal' => [
         'title' => '成功创建令牌',
         'description' => '这是您新创建的 API 令牌。请记录令牌的值，此后你将无法再次看到它。',
-        'button' => '好的，我已经拷贝了'
-    ]
+        'button' => '好的，我已经拷贝了',
+    ],
 ];
