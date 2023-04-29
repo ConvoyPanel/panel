adc35ad95b286961092096556a6f01fe4b5eb449
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-04-28 21:48:52 -0500

    New translations tokens.php (English)

--- lang/en/tokens.php
+++ lang/en/tokens.php
@@ -2,11 +2,11 @@
 
 return [
     'create_modal' => [
-        'title' => 'Create a Token'
+        'title' => 'Create a Token',
     ],
     'created_modal' => [
         'title' => 'Token Created',
         'description' => 'Here is your newly created API token. Please take note of the token\'s value as this is the only and last time you will see it.',
-        'button' => 'Okay, I copied it'
-    ]
+        'button' => 'Okay, I copied it',
+    ],
 ];
