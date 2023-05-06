1cb6e1175e473cbf387b465f4b7de16cad1b93bb
Eric Wang <eric.wang401@gmail.com>
2023-05-06 21:41:34 +0000

    refactor locations page + add translations for it

---
+++ app/Transformers/Coterm/NoVncCredentialsTransformer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Convoy\Transformers\Coterm;
+
+use League\Fractal\TransformerAbstract;
+
+class NoVncCredentialsTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected array $defaultIncludes = [
+        //
+    ];
+    
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected array $availableIncludes = [
+        //
+    ];
+    
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform()
+    {
+        return [
+            //
+        ];
+    }
+}
