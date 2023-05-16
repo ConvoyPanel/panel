f3dd6ec78828b79be05d2b7e416964e3e53356fc
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-15 23:12:20 -0500

    New translations settings.php (Afrikaans)

--- lang/af/settings.php
+++ lang/af/settings.php
@@ -1,42 +1,15 @@
 <?php
 
 return [
-    'ip_allocation' => [
-        'empty_state' => 'There are no addresses associated with this server.',
-    ],
-    'display_info' => [
-        'title' => 'Server Name',
-    ],
-    'reinstall' => [
-        'title' => 'Reinstall Server',
-        'description' => 'Start your server on a fresh slate.',
-        'start_server_after_installing' => 'Start Server After Completing Installation',
-        'modal' => [
-            'title' => 'Confirm Reinstallation',
-            'description' => 'Are you sure you want to reinstall this server? All data will be lost.',
-        ],
-    ],
-    'isos' => [
-        'title' => 'Mountable ISOs',
-        'empty' => 'There are no ISOs',
-    ],
-    'device_config' => [
-        'title' => 'Device Configuration',
-        'current' => 'Current Boot Order (the highest will be used first)',
-        'unused' => 'Unused Devices',
-        'unused_empty' => 'There are no unused devices.',
-        'no_boot_device_warning' => 'No boot device has been configured. Your VM will not start.',
-    ],
-    'nameservers' => [
-        'title' => 'Nameservers',
-        'nameserver' => 'Nameserver :index',
-        'add' => 'New Nameserver',
-    ],
-    'auth' => [
-        'title' => 'Authentication',
-    ],
-    'hardware' => [
-        'bandwidth_used' => 'Bandwidth Used',
-        'bandwidth_alloted' => 'Bandwidth Alloted',
-    ],
+    'server_info' => [
+        'title' => 'Server Information',
+        'statuses' => [
+            'ready' => 'Ready',
+            'installing' => 'Installation In Progress',
+            'install_failed' => 'Recent Installation Failed',
+            'suspended' => 'Suspended',
+            'restoring_backup' => 'Restoring From a Backup',
+            'restoring_snapshot' => 'Restoring From a Snapshot',
+        ]
+    ]
 ];
