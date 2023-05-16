f51e1cddbff89b6f312523b53f0d8f120cc65cb8
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-15 23:12:17 -0500

    New translations settings.php (Spanish)

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -1,42 +1,15 @@
 <?php
 
 return [
-    'ip_allocation' => [
-        'empty_state' => 'No hay direcciones asociadas a este servidor.',
-    ],
-    'display_info' => [
-        'title' => 'Nombre del Servidor',
-    ],
-    'reinstall' => [
-        'title' => 'Reinstalar Servidor',
-        'description' => 'Empiece su servidor desde cero.',
-        'start_server_after_installing' => 'Start Server After Completing Installation',
-        'modal' => [
-            'title' => 'Confirmar Reinstalación',
-            'description' => '¿Estás seguro de que quieres reinstalar este servidor? Se perderán todos los datos.',
-        ],
-    ],
-    'isos' => [
-        'title' => 'ISOs Disponibles',
-        'empty' => 'No hay ISOs',
-    ],
-    'device_config' => [
-        'title' => 'Configuración del Dispositivo',
-        'current' => 'Orden de Arranque Actual (se utilizará primero el más alto)',
-        'unused' => 'Dispositivos no Utilizados',
-        'unused_empty' => 'No hay dispositivos sin utilizar.',
-        'no_boot_device_warning' => 'No se ha configurado ningún dispositivo de arranque. Su VM no se iniciará.',
-    ],
-    'nameservers' => [
-        'title' => 'Nameservers',
-        'nameserver' => 'Nameserver :index',
-        'add' => 'Nuevo Servidor de Nombres',
-    ],
-    'auth' => [
-        'title' => 'Autentificación',
-    ],
-    'hardware' => [
-        'bandwidth_used' => 'Ancho de Banda Usado',
-        'bandwidth_alloted' => 'Ancho de Banda Asignado',
-    ],
+    'server_info' => [
+        'title' => 'Server Information',
+        'statuses' => [
+            'ready' => 'Ready',
+            'installing' => 'Installation In Progress',
+            'install_failed' => 'Recent Installation Failed',
+            'suspended' => 'Suspended',
+            'restoring_backup' => 'Restoring From a Backup',
+            'restoring_snapshot' => 'Restoring From a Snapshot',
+        ]
+    ]
 ];
