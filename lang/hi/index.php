062b341f86b44cf54a30282e15a5c0d88a7ca93a
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-19 16:27:01 -0500

    New translations index.php (Hindi)

--- lang/hi/index.php
+++ lang/hi/index.php
@@ -1,34 +1,7 @@
 <?php
 
 return [
-    'create_server' => 'Create Server',
-    'backup_limit' => 'Backup Limit',
-    'bandwidth_limit' => 'Bandwidth Limit',
-    'bandwidth_usage' => 'Bandwidth Usage',
-    'snapshot_limit' => 'Snapshots Limit',
-    'limit_placeholder' => 'Leave blank for no limit',
-    'should_create_vm' => 'Should Create VM',
-    'start_server_after_installing' => 'Start Server After Completing Installation',
-    'vmid_placeholder' => 'Leave blank for random VMID',
-    'create_modal' => [
-        'title' => 'Create a Server',
-    ],
-    'manage_server' => 'Manage Server',
-    'server_build' => [
-        'title' => 'Server Build',
-    ],
-    'server_info' => [
-        'title' => 'Server Information',
-    ],
-    'suspension' => [
-        'title' => 'Suspension',
-        'description' => 'Toggle the suspension status of the server.',
-        'not_suspended' => 'This server isn\'t suspended',
-        'suspended' => 'This server is suspended',
-    ],
-    'delete_server' => [
-        'title' => 'Delete Server',
-        'description' => 'The server will be deleted from Convoy. Backups and other associated data will be destroyed. However, you can tick the checkbox below to keep the virtual machine and data on the Proxmox node.',
-        'dont_purge' => 'Do not purge VM and related files',
-    ],
+    'show_all_servers' => 'सभी सर्वर प्रदर्शित करें',
+    'no_servers_admin_empty_state' => 'कोई उपलब्ध सर्वर नहीं',
+    'no_servers_empty_state' => 'आपको कोई सर्वर असाइन नहीं किया गया है',
 ];
