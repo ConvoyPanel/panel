01d4e8e2aa9ec5e99fdbf3feed1534d1071cec45
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-18 23:25:34 -0500

    New translations index.php (Chinese Traditional)

--- lang/zh/index.php
+++ lang/zh/index.php
@@ -2,33 +2,33 @@
 
 return [
     'create_server' => 'Create Server',
-    'backup_limit' => '备份限制',
-    'bandwidth_limit' => '带宽限制',
+    'backup_limit' => 'Backup Limit',
+    'bandwidth_limit' => 'Bandwidth Limit',
     'bandwidth_usage' => 'Bandwidth Usage',
     'snapshot_limit' => 'Snapshots Limit',
     'limit_placeholder' => 'Leave blank for no limit',
-    'should_create_vm' => '是否创建虚拟机',
+    'should_create_vm' => 'Should Create VM',
     'start_server_after_installing' => 'Start Server After Completing Installation',
     'vmid_placeholder' => 'Leave blank for random VMID',
     'create_modal' => [
-        'title' => '创建服务器',
+        'title' => 'Create a Server',
     ],
-    'manage_server' => '管理服务器',
+    'manage_server' => 'Manage Server',
     'server_build' => [
-        'title' => '服务器构建',
+        'title' => 'Server Build',
     ],
     'server_info' => [
-        'title' => '服务器信息',
+        'title' => 'Server Information',
     ],
     'suspension' => [
-        'title' => '暂停服务器',
-        'description' => '切换服务器是否处于暂停状态。',
-        'not_suspended' => '服务器尚未被暂停。',
-        'suspended' => '服务器已被暂停。',
+        'title' => 'Suspension',
+        'description' => 'Toggle the suspension status of the server.',
+        'not_suspended' => 'This server isn\'t suspended',
+        'suspended' => 'This server is suspended',
     ],
     'delete_server' => [
-        'title' => '删除服务器',
-        'description' => '此操作将服务器从 Convoy 中删除。备份和其他相关数据将被销毁。通过勾选下面的复选框，你仍可以将虚拟机和数据保存在 Proxmox 节点上。',
-        'dont_purge' => '不要彻底删除虚拟机及相关文件',
+        'title' => 'Delete Server',
+        'description' => 'The server will be deleted from Convoy. Backups and other associated data will be destroyed. However, you can tick the checkbox below to keep the virtual machine and data on the Proxmox node.',
+        'dont_purge' => 'Do not purge VM and related files',
     ],
 ];
