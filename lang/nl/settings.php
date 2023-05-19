ef894dd836d4a3da13ea234977e6d96e991af25d
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-18 23:25:17 -0500

    New translations settings.php (Dutch)

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -11,5 +11,31 @@ return [
             'restoring_backup' => 'Restoring From a Backup',
             'restoring_snapshot' => 'Restoring From a Snapshot',
         ]
+    ],
+    'suspension' => [
+        'title' => 'Suspension',
+        'description' => 'Toggle the suspension status of the server.',
+        'statuses' => [
+            'suspended' => 'This server is suspended.',
+            'not_suspended' => 'This server isn\'t suspended.'
+        ],
+        'suspend' => 'Suspend',
+        'unsuspend' => 'Unsuspend',
+    ],
+    'deletion' => [
+        'title' => 'Delete Server',
+        'description' => 'The server will be deleted from Convoy. Backups and other associated data will be
+                                destroyed. However, you can tick the checkbox below to keep the virtual machine and data
+                                on the Proxmox node.',
+        'deleting_status' => 'This server is currently being deleted.',
+        'no_purge' => 'Do not purge VM and related files',
+        'confirmation' => [
+            'title' => 'Delete :name',
+            'description' => 'Are you sure you want to delete :name?'
+        ]
+    ],
+    'build' => [
+        'title' => 'Server Build',
+
     ]
 ];
