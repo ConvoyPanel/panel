df8e067296391cac1b4e22722acdf55304553b6b
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-19 16:27:39 -0500

    New translations overview.php (Spanish)

--- lang/es/overview.php
+++ lang/es/overview.php
@@ -1,5 +1,38 @@
 <?php
 
 return [
-  'enter_server_console' => 'Enter Server Console',
+    'power_actions' => [
+        'start' => 'Iniciar',
+        'restart' => 'Reiniciar',
+        'kill' => 'Terminar',
+        'shutdown' => 'Apagar',
+    ],
+    'notices' => [
+        'power_action_sent_success' => 'Acción enviada con éxito. Puede tardar un momento en procesarse.',
+        'power_action_sent_fail' => 'No se ha podido enviar la acción de energia al servidor.',
+    ],
+
+    'state' => 'Estado',
+    'states' => [
+        'stopped' => 'Detenido',
+        'running' => 'Ejecutando',
+        'stopping' => 'Deteniendo',
+        'starting' => 'Iniciando',
+        'shutting_down' => 'Apagando',
+    ],
+    'uptime' => 'Tiempo de Actividad',
+    'poll_status_error' => 'Fallo al sondear el estado del servidor. Reintentando en 5 segundos...',
+
+    'terminal' => [
+        'title' => 'Consola',
+        'description' => 'Gestiona remotamente tu servidor desde la web.',
+        'novnc_description' => 'El mejor por compatibilidad, pero carece de prestaciones y rendimiento.',
+        'xtermjs_description' => 'Lo mejor para el rendimiento, pero no funciona para todos los sistemas operativos.',
+    ],
+
+    'server_config' => [
+        'title' => 'Configurar este Servidor',
+        'description' => '¡Usted es un administrador! Puede hacer clic a continuación para visitar la configuración de construcción de este servidor y realizar modificaciones.',
+        'configure_server' => 'Configurar Servidor',
+    ],
 ];
