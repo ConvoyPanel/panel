51dad8225eb6c0876acc11f987f4b101c00de7b5
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-19 16:25:10 -0500

    New translations index.php (Chinese Simplified)

--- lang/zh/index.php
+++ lang/zh/index.php
@@ -1,41 +1,34 @@
 <?php
 
 return [
-    'create_node' => 'Create Node',
+    'create_server' => 'Create Server',
+    'backup_limit' => '备份限制',
+    'bandwidth_limit' => '带宽限制',
+    'bandwidth_usage' => 'Bandwidth Usage',
+    'snapshot_limit' => 'Snapshots Limit',
+    'limit_placeholder' => 'Leave blank for no limit',
+    'should_create_vm' => '是否创建虚拟机',
+    'start_server_after_installing' => 'Start Server After Completing Installation',
+    'vmid_placeholder' => 'Leave blank for random VMID',
     'create_modal' => [
-        'title' => 'Create a Node',
+        'title' => '创建服务器',
     ],
-
-    'location_group' => 'Location Group',
-    'pve_name' => 'Node Name In Proxmox',
-    'override_creds' => 'Override credentials',
-    'creds_warning' => 'Please disable privilege separation and grant root privileges.',
-    'token_id' => 'Token ID',
-    'secret' => 'Secret',
-    'port' => 'Port',
-    'memory_allocation' => 'Memory Allocation',
-    'memory_overallocation' => 'Memory Overallocation',
-    'disk_allocation' => 'Disk Allocation',
-    'disk_overallocation' => 'Disk Overallocation',
-    'vm_storage' => 'VM Storage',
-    'backup_storage' => 'Backup Storage',
-    'iso_storage' => 'ISO Storage',
-
-    'create_template_modal' => [
-        'title' => 'Create a Template',
+    'manage_server' => '管理服务器',
+    'server_build' => [
+        'title' => '服务器构建',
     ],
-    'new_template' => 'New Template',
-    'create_template_group_modal' => [
-        'title' => 'Create a Template Group',
+    'server_info' => [
+        'title' => '服务器信息',
     ],
-    'new_template_group' => 'New Template Group',
-
-    'node_info' => [
-        'title' => 'Node Information',
+    'suspension' => [
+        'title' => '暂停服务器',
+        'description' => '切换服务器是否处于暂停状态。',
+        'not_suspended' => '服务器尚未被暂停。',
+        'suspended' => '服务器已被暂停。',
     ],
-    'delete_node' => [
-        'title' => 'Delete Node',
-        'description' => 'The node will be permanently deleted from Convoy. This action is irreversible and can not be undone.',
-        'has_servers_error' => 'You cannot delete a node that has servers assigned to it.',
+    'delete_server' => [
+        'title' => '删除服务器',
+        'description' => '此操作将服务器从 Convoy 中删除。备份和其他相关数据将被销毁。通过勾选下面的复选框，你仍可以将虚拟机和数据保存在 Proxmox 节点上。',
+        'dont_purge' => '不要彻底删除虚拟机及相关文件',
     ],
 ];
