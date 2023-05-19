13268289176e19d64a8c79036ebf77cf8c9432e6
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-19 16:26:56 -0500

    New translations index.php (Chinese Simplified)

--- lang/zh/index.php
+++ lang/zh/index.php
@@ -1,34 +1,7 @@
 <?php
 
 return [
-    'create_server' => 'Create Server',
-    'backup_limit' => 'Backup Limit',
-    'bandwidth_limit' => 'Bandwidth Limit',
-    'bandwidth_usage' => 'Bandwidth Usage',
-    'snapshot_limit' => 'Snapshots Limit',
-    'limit_placeholder' => 'Leave blank for no limit',
-    'should_create_vm' => 'Should Create VM',
-    'start_server_after_installing' => 'Start Server After Completing Installation',
-    'vmid_placeholder' => 'Leave blank for random VMID',
-    'create_modal' => [
-        'title' => 'Create a Server',
-    ],
-    'manage_server' => 'Manage Server',
-    'server_build' => [
-        'title' => 'Server Build',
-    ],
-    'server_info' => [
-        'title' => 'Server Information',
-    ],
-    'suspension' => [
-        'title' => 'Suspension',
-        'description' => 'Toggle the suspension status of the server.',
-        'not_suspended' => 'This server isn\'t suspended',
-        'suspended' => 'This server is suspended',
-    ],
-    'delete_server' => [
-        'title' => 'Delete Server',
-        'description' => 'The server will be deleted from Convoy. Backups and other associated data will be destroyed. However, you can tick the checkbox below to keep the virtual machine and data on the Proxmox node.',
-        'dont_purge' => 'Do not purge VM and related files',
-    ],
+    'show_all_servers' => '显示所有服务器',
+    'no_servers_admin_empty_state' => '这里还没有服务器可供管理。',
+    'no_servers_empty_state' => '你尚未拥有任何服务器。',
 ];
