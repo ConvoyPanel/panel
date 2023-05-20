ee7bd49194d6ddf84c9b8e312ee001ef94e39c58
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-20 15:40:47 -0500

    New translations locations.php (Hindi)

---
+++ lang/hi-IN/admin/locations.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'short_code' => 'चरित्र पहचानकर्ता',
+    'create_location' => 'Create Location',
+    'create_modal' => [
+        'title' => 'Create a Location',
+    ],
+];
