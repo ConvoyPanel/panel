f013e622f5c7ea7ed2775b5d204243f211c026f3
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-20 15:44:15 -0500

    New translations passwords.php (Spanish)

---
+++ lang/es-ES/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => '¡Su contraseña ha sido restablecida!',
+    'sent' => '¡Te hemos enviado por correo electrónico el enlace para restablecer tu contraseña!',
+    'throttled' => 'Por favor, espere antes de reintentarlo.',
+    'token' => 'Este token de restablecimiento de contraseña no es válido.',
+    'user' => "No podemos encontrar un usuario con esa dirección de email.",
+
+];
