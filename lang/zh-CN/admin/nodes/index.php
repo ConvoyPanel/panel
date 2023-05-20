9486f48c3af7c364e6bdee908ee6f7972c891be9
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-20 15:39:27 -0500

    New translations index.php (Chinese Simplified)

---
+++ lang/zh-CN/admin/nodes/index.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    'create_node' => 'Create Node',
+    'create_modal' => [
+        'title' => '创建节点',
+    ],
+
+    'location_group' => '地域分组',
+    'pve_name' => 'Proxmox 中的节点名称',
+    'override_creds' => '覆盖凭证',
+    'creds_warning' => 'Please disable privilege separation and grant root privileges.',
+    'token_id' => '令牌 ID',
+    'secret' => '令牌密钥',
+    'port' => '端口',
+    'memory_allocation' => '内存分配',
+    'memory_overallocation' => '内存超开',
+    'disk_allocation' => '硬盘分配',
+    'disk_overallocation' => '硬盘超开',
+    'vm_storage' => '虚拟机存储',
+    'backup_storage' => '备份存储',
+    'iso_storage' => 'ISO 存储',
+
+    'create_template_modal' => [
+        'title' => '创建模板',
+    ],
+    'new_template' => '新建模板',
+    'create_template_group_modal' => [
+        'title' => '创建模板分组',
+    ],
+    'new_template_group' => '新建模板分组',
+
+    'node_info' => [
+        'title' => '节点信息',
+    ],
+    'delete_node' => [
+        'title' => '删除节点',
+        'description' => '节点将从 Convoy 中永久删除。此操作不能被复原及撤销。',
+        'has_servers_error' => '你无法删除一个分配有服务器的节点。',
+    ],
+];
