5b3fab198f773a7472208d9864c85ed6a2c236d8
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-20 15:42:09 -0500

    New translations tokens.php (Spanish)

---
+++ lang/es-ES/admin/tokens.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'create_modal' => [
+        'title' => 'Crear un Token',
+    ],
+    'created_modal' => [
+        'title' => 'Token Creado',
+        'description' => 'Aquí está su token de API recién creado. Tenga en cuenta el valor del token, ya que es la única y última vez que lo verá.',
+        'button' => 'Vale, lo he copiado',
+    ],
+];
