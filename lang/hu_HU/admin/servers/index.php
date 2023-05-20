75406e878c2e8661dda4a3e13c84cc5f9bff035d
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-20 16:08:19 -0500

    New translations index.php (Hungarian)

---
+++ lang/hu_HU/admin/servers/index.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'create_server' => 'Create Server',
+    'backup_limit' => 'Backup Limit',
+    'bandwidth_limit' => 'Bandwidth Limit',
+    'bandwidth_usage' => 'Bandwidth Usage',
+    'snapshot_limit' => 'Snapshots Limit',
+    'limit_placeholder' => 'Leave blank for no limit',
+    'should_create_vm' => 'Should Create VM',
+    'start_server_after_installing' => 'Start Server After Completing Installation',
+    'vmid_placeholder' => 'Leave blank for random VMID',
+    'create_modal' => [
+        'title' => 'Create a Server',
+    ],
+    'manage_server' => 'Manage Server',
+    'server_build' => [
+        'title' => 'Server Build',
+    ],
+    'server_info' => [
+        'title' => 'Server Information',
+    ],
+    'suspension' => [
+        'title' => 'Suspension',
+        'description' => 'Toggle the suspension status of the server.',
+        'not_suspended' => 'This server isn\'t suspended',
+        'suspended' => 'This server is suspended',
+    ],
+    'delete_server' => [
+        'title' => 'Delete Server',
+        'description' => 'The server will be deleted from Convoy. Backups and other associated data will be destroyed. However, you can tick the checkbox below to keep the virtual machine and data on the Proxmox node.',
+        'dont_purge' => 'Do not purge VM and related files',
+    ],
+];
