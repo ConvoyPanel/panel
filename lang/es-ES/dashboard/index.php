7aed2d8590b4f8ca7862826275f5fd425c70be8f
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-20 15:43:26 -0500

    New translations index.php (Spanish)

---
+++ lang/es-ES/dashboard/index.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'show_all_servers' => 'Mostrar todos los servidores',
+    'no_servers_admin_empty_state' => 'No hay servidores.',
+    'no_servers_empty_state' => 'No tienes servidores.',
+];
