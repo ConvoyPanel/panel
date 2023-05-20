5b3760ed5faf0192665a604b7c3cf4803be8932c
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-20 15:42:34 -0500

    New translations users.php (Spanish)

---
+++ lang/es-ES/admin/users.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'new_user' => 'Nuevo Usuario',
+    'create_modal' => [
+        'title' => 'Crear Usuario',
+        'is_admin_warning' => 'Está dando a este usuario privilegios administrativos.',
+    ],
+    'user_info' => [
+        'title' => 'Información del Usuario',
+        'override_pass' => 'Anular contraseña',
+    ],
+    'delete_user' => [
+        'title' => 'Eliminar Usuario',
+        'description' => 'El usuario y sus datos asociados serán eliminados.',
+        'has_servers_error' => 'Este usuario no puede ser eliminado con servidores aún asociados a él.',
+    ],
+];
