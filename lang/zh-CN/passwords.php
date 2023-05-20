55c8e551f9bc3a7e4d1a91985116595fdbaaa7e9
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-20 15:44:33 -0500

    New translations passwords.php (Chinese Simplified)

---
+++ lang/zh-CN/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => '你的密码已被重置！',
+    'sent' => '我们已向该账户的邮箱发送了密码重置链接！',
+    'throttled' => '请在重试前稍作等待。',
+    'token' => '该密码重置令牌无效。',
+    'user' => "我们无法找到与该邮箱相关的用户。",
+
+];
