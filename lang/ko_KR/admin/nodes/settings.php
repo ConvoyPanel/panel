e73fa7ac9b3834bbf3bfe794b89ff150411ac37a
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-21 09:15:26 -0500

    New translations settings.php (Korean)

--- lang/ko_KR/admin/nodes/settings.php
+++ lang/ko_KR/admin/nodes/settings.php
@@ -9,6 +9,16 @@ return [
         'description' => 'Coterm lets you hide your node\'s origin from your users while they are accessing the web console.',
         'enable' => 'Enable Coterm',
         'tls' => 'TLS/SSL Enabled',
-        'regenerate' => 'Regenerate Token',
+        'token_created' => [
+            'title' => 'Token Created',
+            'description' => 'Here is your newly created token. Please take note of the token\'s value as this is the only and
+            last time you will see it.',
+            'action' => 'Okay, I got it'
+        ],
+        'reset' => [
+            'title' => 'Reset Token?',
+            'description' => 'Are you sure you want to reset this node\'s Coterm token? Any Coterm instances that are relying on this token will fail.',
+            'action' => 'Reset Token',
+        ]
     ]
 ];
