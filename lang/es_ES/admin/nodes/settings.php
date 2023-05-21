04a3803cf1660feb3d9d95b3c158766ca71500d2
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-20 23:46:44 -0500

    New translations settings.php (Spanish)

--- lang/es_ES/admin/nodes/settings.php
+++ lang/es_ES/admin/nodes/settings.php
@@ -1,7 +1,14 @@
 <?php
 
 return [
-  'node_info' => [
-      'title' => 'Node Information',
-  ]
+    'node_info' => [
+        'title' => 'Node Information',
+    ],
+    'coterm' => [
+        'title' => 'Coterm',
+        'description' => 'Coterm lets you hide your node\'s origin from your users while they are accessing the web console.',
+        'enable' => 'Enable Coterm',
+        'tls' => 'TLS/SSL Enabled',
+        'regenerate' => 'Regenerate Token',
+    ]
 ];
