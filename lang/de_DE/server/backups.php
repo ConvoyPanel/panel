936f9bd7b4789bfc80f0569b0cb19a26eb989e17
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-22 07:57:08 -0500

    New translations backups.php (German)

--- lang/de_DE/server/backups.php
+++ lang/de_DE/server/backups.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'create_backup' => 'Create Backup',
-    'empty_state' => 'There are no backups',
-    'counter_tooltip' => 'You\'ve made :count out of :max backups',
+    'create_backup' => 'Backup erstellen',
+    'empty_state' => 'Es gibt keine Backups',
+    'counter_tooltip' => 'Du hast :count von :max Backups erstellt',
     'create_modal' => [
-        'title' => 'Create a Backup',
+        'title' => 'Ein Backup erstellen',
         'description' => 'Creating a backup will take a copy of your server files. This can take a while depending on the size of your server.',
         'compression_type' => 'Compression Type',
-        'mode' => 'Mode',
+        'mode' => 'Typ',
         'modes' => [
             'snapshot' => 'Snapshot',
             'suspend' => 'Suspend',
@@ -20,7 +20,7 @@ return [
         'description' => 'Are you sure you want to delete this backup?',
     ],
     'restore_modal' => [
-        'title' => 'Restore From :name',
+        'title' => 'Wiederherstellen von :name',
         'description' => 'Are you sure you want to restore from this backup?',
     ],
     'notices' => [
