40159baf3a579041d73b08c8038ceb46a9a54dda
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-22 07:57:07 -0500

    New translations settings.php (German)

--- lang/de_DE/admin/servers/settings.php
+++ lang/de_DE/admin/servers/settings.php
@@ -4,8 +4,8 @@ return [
     'server_info' => [
         'title' => 'Server Information',
         'statuses' => [
-            'ready' => 'Ready',
-            'installing' => 'Installation In Progress',
+            'ready' => 'Bereit',
+            'installing' => 'Installation in Arbeit',
             'install_failed' => 'Recent Installation Failed',
             'suspended' => 'Suspended',
             'restoring_backup' => 'Restoring From a Backup',
