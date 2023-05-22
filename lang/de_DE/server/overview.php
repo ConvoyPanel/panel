b9b516c254ae9d2db00864846fd49e773d0294e2
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-05-22 09:50:36 -0500

    New translations overview.php (German)

--- lang/de_DE/server/overview.php
+++ lang/de_DE/server/overview.php
@@ -2,37 +2,37 @@
 
 return [
     'power_actions' => [
-        'start' => 'Start',
-        'restart' => 'Restart',
-        'kill' => 'Kill',
-        'shutdown' => 'Shutdown',
+        'start' => 'Starten',
+        'restart' => 'Neustarten',
+        'kill' => 'Aus',
+        'shutdown' => 'Herunterfahren',
     ],
     'notices' => [
-        'power_action_sent_success' => 'Successfully sent power action. It may take a moment to process.',
-        'power_action_sent_fail' => 'Failed to send power action.',
+        'power_action_sent_success' => 'Der Befehl wurde erfolgreich gesendet. Es kann einen Moment dauern, bis er verarbeitet ist.',
+        'power_action_sent_fail' => 'Befehl fehlgeschlagen.',
     ],
 
-    'state' => 'State',
+    'state' => 'Status',
     'states' => [
-        'stopped' => 'Stopped',
-        'running' => 'Running',
-        'stopping' => 'Stopping',
-        'starting' => 'Starting',
-        'shutting_down' => 'Shutting Down',
+        'stopped' => 'Gestoppt',
+        'running' => 'Läuft',
+        'stopping' => 'Stoppt',
+        'starting' => 'Startet',
+        'shutting_down' => 'Fährt herunter',
     ],
-    'uptime' => 'Uptime',
-    'poll_status_error' => 'Failed to poll server status. Retrying in 5 seconds...',
+    'uptime' => 'Betriebszeit',
+    'poll_status_error' => 'Der Serverstatus konnte nicht abgefragt werden. Versuche in 5 Sekunden erneut...',
 
     'terminal' => [
-        'title' => 'Terminal',
-        'description' => 'Remotely manage your server from the web.',
-        'novnc_description' => 'Best for compatibility but lacks features and performance.',
-        'xtermjs_description' => 'Best for performance but doesn\'t work for every operating system.',
+        'title' => 'Konsole',
+        'description' => 'Verwalte Deinen Server per Fernzugriff.',
+        'novnc_description' => 'Am besten für Kompatibilität geeignet, aber Funktionen und Leistung sind eventuell eingeschränkt.',
+        'xtermjs_description' => 'Am besten für Leistung geeignet, aber nicht von jedem Betriebssystem unterstützt.',
     ],
 
     'server_config' => [
-        'title' => 'Configure This Server',
-        'description' => 'You are an administrator! You can click below to visit this server\'s build configuration and make edits.',
-        'configure_server' => 'Configure Server',
+        'title' => 'Server konfigurieren',
+        'description' => 'Du bist ein Administrator! Du kannst mit einem Klick unten die Konfiguration des Servers ansehen und bearbeiten.',
+        'configure_server' => 'Server konfigurieren',
     ],
 ];
