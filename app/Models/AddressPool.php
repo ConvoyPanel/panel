d080a2a93d3a9a782d17c5e1d66c21fd16eb95d4
Eric Wang <eric.wang401@gmail.com>
2023-05-28 15:31:58 +0000

    rename IPAddress to Address, add AddressPool, use JWT for SSO Token

---
+++ app/Models/AddressPool.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Convoy\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AddressPool extends Model
+{
+    use HasFactory;
+}
