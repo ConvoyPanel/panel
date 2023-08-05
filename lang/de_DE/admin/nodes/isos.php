1411da3484e767e2a7bcad91db08b68eb772540f
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-08-05 13:11:07 -0500

    New translations isos.php (German)

--- lang/de_DE/admin/nodes/isos.php
+++ lang/de_DE/admin/nodes/isos.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'create_iso' => 'Create ISO',
-    'checksum_algorithm' => 'Checksum Algorithm',
-    'checksum' => 'Checksum',
+    'create_iso' => 'ISO erstellen',
+    'checksum_algorithm' => 'Prüfsummen-Algorithmus',
+    'checksum' => 'Prüfsumme',
     'create_modal' => [
-        'title' => 'Create an ISO',
-        'non_iso_file_name_error' => 'The file extension must end in .iso',
-        'fail_to_query_remote_file_error' => 'Failed to query remote file.',
+        'title' => 'Erstelle eine ISO',
+        'non_iso_file_name_error' => 'Die Dateiendung muss ".iso" sein',
+        'fail_to_query_remote_file_error' => 'Abfrage der Remote-Datei fehlgeschlagen.',
     ],
 ];
