572cfab0435c7ff5aae8a829261c96c91b9c876d
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-08-05 16:20:56 -0500

    New translations passwords.php (German)

--- lang/de_DE/passwords.php
+++ lang/de_DE/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Dein Passwort wurde zurückgesetzt!',
+    'sent' => 'Wir haben die einen Link zum Zurücksetzen Deines Passworts per Email geschickt!',
+    'throttled' => 'Bitte warte bevor Du es erneut versuchst.',
+    'token' => 'Dieser Passwort-Token ist nicht gültig.',
+    'user' => "Wir konnten keinen Benutzer mit dieser Email-Adresse finden.",
 
 ];
