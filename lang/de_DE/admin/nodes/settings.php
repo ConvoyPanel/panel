e4ca8a4fa82f90c6ab0e1cfaf3a043e9cc0e3e00
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-08-05 16:20:50 -0500

    New translations settings.php (German)

--- lang/de_DE/admin/nodes/settings.php
+++ lang/de_DE/admin/nodes/settings.php
@@ -6,17 +6,17 @@ return [
     ],
     'coterm' => [
         'title' => 'Coterm',
-        'description' => 'Mit Coterm kannst du die IP deiner Node vor Benutzern verbergen, während diese auf die Webkonsole zugreifen.',
+        'description' => 'Mit Coterm kannst Du die IP deiner Node vor Benutzern verbergen, während diese auf die Webkonsole zugreifen.',
         'enable' => 'Coterm aktivieren',
         'tls' => 'SSL/TLS aktiviert',
         'token_created' => [
             'title' => 'Token erstellt',
-            'description' => 'Hier ist dein neu erstellter Token. Bitte notiere ihn dir, da dieser nie wieder angezeigt werden kann.',
+            'description' => 'Hier ist Dein neu erstellter Token. Bitte notiere ihn Dir, da dieser nie wieder angezeigt werden kann.',
             'action' => 'Okay, ich habe es verstanden'
         ],
         'reset' => [
             'title' => 'Token zurücksetzen?',
-            'description' => 'Bist du sicher, dass du den Coterm-Token dieser Node zurücksetzen möchtest? Alle Coterm-Instanzen, die diesen Token verwenden, werden nicht mehr funktionieren.',
+            'description' => 'Bist Du sicher, dass Du den Coterm-Token dieser Node zurücksetzen möchtest? Alle Coterm-Instanzen, die diesen Token verwenden, werden nicht mehr funktionieren.',
             'action' => 'Token zurücksetzen',
         ]
     ]
