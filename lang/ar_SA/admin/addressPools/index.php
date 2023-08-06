d769e8003170b672589f4c636e18dfb17af3a94b
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-08-06 12:49:18 -0500

    New translations index.php (Arabic)

--- lang/ar_SA/admin/addressPools/index.php
+++ lang/ar_SA/admin/addressPools/index.php
@@ -2,5 +2,13 @@
 
 return [
     'create_pool' => 'Create Pool',
+    'delete_modal' => [
+        'title' => 'Delete :name?',
+        'description' => 'Are you sure you want to delete this pool?',
+        'nodes_linked_error' => 'This pool cannot be deleted until all nodes are unlinked.'
+    ],
+    'edit_modal' => [
+      'title' => 'Editing :name',
+    ],
     'nodes_nothing_found' => 'No nodes found',
 ];
