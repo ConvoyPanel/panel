beb8e17f4cc7c2c9fcf68e3cd9709dcbbed8fdfb
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-08-06 13:52:27 -0500

    New translations index.php (German)

--- lang/de_DE/admin/addressPools/index.php
+++ lang/de_DE/admin/addressPools/index.php
@@ -3,12 +3,12 @@
 return [
     'create_pool' => 'Pool erstellen',
     'delete_modal' => [
-        'title' => 'Delete :name?',
-        'description' => 'Are you sure you want to delete this pool?',
-        'nodes_linked_error' => 'This pool cannot be deleted until all nodes are unlinked.'
+        'title' => ':name Löschen?',
+        'description' => 'Bist Du sicher, dass Du diesen Pool löschen möchtest?',
+        'nodes_linked_error' => 'Dieser Pool kann nicht gelöscht werden, bis alle Nodes getrennt werden.'
     ],
     'edit_modal' => [
-      'title' => 'Editing :name',
+      'title' => 'Bearbeite :name',
     ],
     'nodes_nothing_found' => 'Keine Nodes gefunden',
 ];
