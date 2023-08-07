0df282dea6c3fe4e7ad240ee201c6662221e0e8b
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-08-06 20:06:20 -0500

    New translations addresses.php (German)

--- lang/de_DE/admin/addressPools/addresses.php
+++ lang/de_DE/admin/addressPools/addresses.php
@@ -3,4 +3,11 @@
 return [
     'create_address' => 'Adresse erstellen',
     'assigned_server' => 'Zugewiesener Server',
+    'edit_modal' => [
+        'title' => 'Editing Address',
+    ],
+    'delete_modal' => [
+        'title' => 'Delete Address?',
+        'description' => 'Are you sure you want to delete :address?',
+    ]
 ];
