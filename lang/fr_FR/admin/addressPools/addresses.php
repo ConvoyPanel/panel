b8f045df3db5ab17c6e3f622a03c49b63098e86d
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-08-06 21:03:00 -0500

    New translations addresses.php (French)

--- lang/fr_FR/admin/addressPools/addresses.php
+++ lang/fr_FR/admin/addressPools/addresses.php
@@ -2,6 +2,12 @@
 
 return [
     'create_address' => 'Create Address',
+    'create_modal' => [
+        'title' => 'Create Address',
+        'bulk_action_unchecked' => 'Bulk Import',
+        'bulk_action_checked_one' => 'Bulk Import (:count address)',
+        'bulk_action_checked_other' => 'Bulk Import (:count addresses)',
+    ],
     'assigned_server' => 'Assigned Server',
     'edit_modal' => [
         'title' => 'Editing Address',
@@ -9,5 +15,5 @@ return [
     'delete_modal' => [
         'title' => 'Delete Address?',
         'description' => 'Are you sure you want to delete :address?',
-    ]
+    ],
 ];
