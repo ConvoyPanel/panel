c0233150e8b5285bed836eaab20ac552861b9fc1
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-09-01 22:10:47 -0500

    New translations strings.php (German)

--- lang/de_DE/strings.php
+++ lang/de_DE/strings.php
@@ -37,7 +37,6 @@ return [
     'suspended' => 'Gesperrt',
     'network' => 'Netzwerk',
     'ip' => 'IP Adresse',
-    'address' => 'Adresse',
     'address_one' => 'Adresse',
     'address_other' => 'Adressen',
     'gateway' => 'Gateway',
@@ -104,4 +103,6 @@ return [
     'cidr' => 'CIDR',
     'ipv4' => 'IPv4',
     'ipv6' => 'IPv6',
+    'single' => 'Single',
+    'multiple' => 'Multiple',
 ];
