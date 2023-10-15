5f556ae95a058c4dffa075299168ffae2f7f7384
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-10-15 03:10:46 -0500

    New translations index.php (Polish)

--- lang/pl_PL/dashboard/index.php
+++ lang/pl_PL/dashboard/index.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'show_all_servers' => 'Show all servers',
-    'no_servers_admin_empty_state' => 'There are no servers.',
-    'no_servers_empty_state' => 'You have no servers.',
+    'show_all_servers' => 'Pokaż wszystkie serwery',
+    'no_servers_admin_empty_state' => 'Nie ma żadnych serwerów.',
+    'no_servers_empty_state' => 'Nie posiadasz żadnych serwerów.',
 ];
