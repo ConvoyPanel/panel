a2b0d2612a53bc469d552b11505c131e9f1f4c9c
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-11-20 20:17:33 -0600

    New translations backups.php (Danish)

--- lang/da_DK/server/backups.php
+++ lang/da_DK/server/backups.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'create_backup' => 'Create Backup',
+    'create_backup' => 'Opret backup',
     'empty_state' => 'There are no backups',
     'counter_tooltip' => 'You\'ve made :count out of :max backups',
     'create_modal' => [
-        'title' => 'Create a Backup',
+        'title' => 'Opret en backup',
         'description' => 'Creating a backup will take a copy of your server files. This can take a while depending on the size of your server.',
         'compression_type' => 'Compression Type',
         'mode' => 'Mode',
