68e8adf32336aeb0976ce08ca961bc07e4faac9b
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-11-20 20:17:31 -0600

    New translations auth.php (Danish)

--- lang/da_DK/auth.php
+++ lang/da_DK/auth.php
@@ -17,7 +17,7 @@ return [
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
-    'sign_in' => 'Sign in',
-    'sign_out' => 'Sign out',
+    'sign_in' => 'Log in',
+    'sign_out' => 'Log ud',
     'sign_in_description' => 'Sign in to your account',
 ];
