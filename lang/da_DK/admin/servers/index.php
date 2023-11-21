530330d55604c20a9b29658b35a8e3aac2a3ef43
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-11-20 19:07:11 -0600

    New translations index.php (Danish)

--- lang/da_DK/admin/servers/index.php
+++ lang/da_DK/admin/servers/index.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    'create_server' => 'Create Server',
+    'create_server' => 'Opret server',
     'backup_limit' => 'Backup Limit',
     'bandwidth_limit' => 'Bandwidth Limit',
-    'bandwidth_usage' => 'Bandwidth Usage',
+    'bandwidth_usage' => 'Båndbredde brugt',
     'snapshot_limit' => 'Snapshots Limit',
     'limit_placeholder' => 'Leave blank for no limit',
     'should_create_vm' => 'Should Create VM',
     'start_server_after_installing' => 'Start Server After Completing Installation',
     'vmid_placeholder' => 'Leave blank for random VMID',
     'create_modal' => [
-        'title' => 'Create a Server',
+        'title' => 'Opret en server',
     ],
-    'manage_server' => 'Manage Server',
+    'manage_server' => 'Administrer server',
     'server_build' => [
         'title' => 'Server Build',
     ],
