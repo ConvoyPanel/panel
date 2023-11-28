42c9fd5e7f109a8766aece2d1cb01c0c5b3309d1
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-11-28 15:30:24 -0600

    Test if admins can access/modify backups of other users' servers

--- tests/Feature/Controllers/Client/Servers/BackupControllerTest.php
+++ tests/Feature/Controllers/Client/Servers/BackupControllerTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Convoy\Models\User;
 use Convoy\Models\Backup;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Queue;
@@ -25,7 +26,7 @@ it('can create backups', function () {
 
     $response->assertOk()
         ->assertJsonPath('data.name', 'Test Backup')
-        ->assertJsonPath('data.is_locked', (int) false);
+        ->assertJsonPath('data.is_locked', 0);
 
     Queue::assertPushed(MonitorBackupJob::class);
 });
@@ -70,3 +71,81 @@ it('can delete backups', function () {
 
     $response->assertNoContent();
 });
+
+describe('admin', function () {
+    it('can create backups', function () {
+        Queue::fake();
+        Http::fake([
+            '*' => Http::response(['data' => 'upid'], 200),
+        ]);
+
+        [$_, $_, $_, $server] = createServerModel();
+
+        $admin = User::factory()->create([
+            'root_admin' => true,
+        ]);
+
+        $response = $this->actingAs($admin)->postJson("/api/client/servers/{$server->uuid}/backups", [
+            'name' => 'Test Backup',
+            'mode' => 'snapshot',
+            'compression_type' => 'none',
+            'is_locked' => false,
+        ]);
+
+        $response->assertOk()
+                 ->assertJsonPath('data.name', 'Test Backup')
+                 ->assertJsonPath('data.is_locked', 0);
+
+        Queue::assertPushed(MonitorBackupJob::class);
+    });
+
+    it('can restore backups', function () {
+        Queue::fake();
+        Http::fake([
+            '*/status/current' => Http::response(file_get_contents(base_path('tests/Fixtures/Repositories/Server/GetStoppedServerStatusData.json')), 200),
+            '*' => Http::response(['data' => 'dummy-upid'], 200),
+
+        ]);
+
+        [$_, $_, $_, $server] = createServerModel();
+
+        $admin = User::factory()->create([
+            'root_admin' => true,
+        ]);
+
+        $backup = Backup::factory()->create([
+            'is_successful' => true,
+            'is_locked' => false,
+            'server_id' => $server->id,
+        ]);
+
+        $response = $this->actingAs($admin)->postJson("/api/client/servers/{$server->uuid}/backups/{$backup->uuid}/restore");
+
+        $response->assertNoContent();
+
+        Queue::assertPushed(MonitorBackupRestorationJob::class);
+    });
+
+    it('can delete backups', function () {
+        Http::fake([
+            '*' => Http::response(['data' => 'dummy-upid'], 200),
+        ]);
+
+        [$_, $_, $_, $server] = createServerModel();
+
+        $admin = User::factory()->create([
+            'root_admin' => true,
+        ]);
+
+        $backup = Backup::factory()->create([
+            'is_successful' => true,
+            'is_locked' => false,
+            'server_id' => $server->id,
+        ]);
+
+        $response = $this->actingAs($admin)->deleteJson("/api/client/servers/{$server->uuid}/backups/{$backup->uuid}");
+
+        $response->assertNoContent();
+    });
+});
+
