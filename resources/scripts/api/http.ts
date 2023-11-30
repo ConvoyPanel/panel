764aa9ed80586f302c645acb69e8f74c3f4ff4ac
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2023-11-29 18:51:47 -0600

    Remove Axios timeout argument

--- resources/scripts/api/http.ts
+++ resources/scripts/api/http.ts
@@ -28,7 +28,6 @@ import axios, { AxiosInstance } from 'axios';
 
 const http: AxiosInstance = axios.create({
     withCredentials: true,
-    timeout: 20000,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
         'Accept': 'application/json',
