d5ad9e6e4de355a4ae098a8826544d4f3b7c3e0f
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2024-01-01 19:46:48 -0600

    Add network helpers

---
+++ app/Helpers/Network.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Convoy\Helpers;
+
+use Convoy\Enums\Network\AddressType;
+use GMP;
+
+function ipv6ToInteger(string $ip): GMP
+{
+    return gmp_import(inet_pton($ip));
+}
+
+function ipv6FromInteger(GMP $integer): ?string
+{
+    $ip = inet_ntop(str_pad(gmp_export($integer), 16, "\0", STR_PAD_LEFT));
+
+    return $ip !== false ? $ip : null;
+}
+
+/**
+ * @return string[]
+ */
+function getAddressesFromRange(AddressType $type, string $from, string $to): array
+{
+    /** @var string[] */
+    $addresses = [];
+
+    if ($type === AddressType::IPV4) {
+        $from = ip2long($from);
+        $to = ip2long($to);
+
+        $addresses[] = $from;
+
+        for ($i = $from; $i <= $to; $i++) {
+            $addresses[] = long2ip($i);
+        }
+    } else {
+        $from = ipv6ToInteger($from);
+        $to = ipv6ToInteger($to);
+
+        $addresses[] = $from;
+
+        for ($i = $from; $i <= $to; $i++) {
+            $addresses[] = ipv6FromInteger($i);
+        }
+    }
+
+    return $addresses;
+}
