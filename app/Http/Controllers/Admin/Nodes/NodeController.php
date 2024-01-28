d1a14f699c4ae4e141e6f799693e42c0f60806bf
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2024-01-28 11:13:07 -0600

    Remove Coterm methods from NodeController.php

--- app/Http/Controllers/Admin/Nodes/NodeController.php
+++ app/Http/Controllers/Admin/Nodes/NodeController.php
@@ -3,14 +3,12 @@
 namespace Convoy\Http\Controllers\Admin\Nodes;
 
 use Convoy\Http\Controllers\ApiController;
-use Convoy\Http\Requests\Admin\Nodes\Settings\UpdateCotermRequest;
 use Convoy\Http\Requests\Admin\Nodes\StoreNodeRequest;
 use Convoy\Http\Requests\Admin\Nodes\UpdateNodeRequest;
 use Convoy\Models\Filters\FiltersNodeWildcard;
 use Convoy\Models\Node;
 use Convoy\Services\Coterm\CotermTokenCreationService;
 use Convoy\Transformers\Admin\NodeTransformer;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
@@ -64,50 +62,6 @@ class NodeController extends ApiController
         $node->update($request->validated());
 
         return fractal($node, new NodeTransformer())->respond();
-    }
-
-    public function updateCoterm(UpdateCotermRequest $request, Node $node)
-    {
-        $payload = $request->validated();
-
-        if ($payload['coterm_enabled'] && (empty($node->coterm_token_id) || empty($node->coterm_token))) {
-            $creds = $this->cotermTokenCreator->handle();
-            $payload['coterm_token_id'] = $creds['token_id'];
-            $payload['coterm_token'] = $creds['token'];
-        }
-
-
-        $node->update($payload);
-
-        return new JsonResponse([
-            'data' => [
-                'is_enabled' => $node->coterm_enabled,
-                'is_tls_enabled' => $node->coterm_tls_enabled,
-                'fqdn' => $node->coterm_fqdn,
-                'port' => $node->coterm_port,
-                'token' => isset($creds) ? "{$node->coterm_token_id}|{$node->coterm_token}" : null,
-            ],
-        ]);
-    }
-
-    public function resetCotermToken(Node $node)
-    {
-        if (!$node->coterm_enabled) {
-            throw new AccessDeniedHttpException('Coterm isn\'t enabled on this node.');
-        }
-
-        $creds = $this->cotermTokenCreator->handle();
-
-        $node->update([
-            'coterm_token_id' => $creds['token_id'],
-            'coterm_token' => $creds['token'],
-        ]);
-
-        return new JsonResponse([
-            'data' => [
-                'token' => "{$node->coterm_token_id}|{$node->coterm_token}",
-            ],
-        ]);
     }
 
     public function destroy(Node $node)
