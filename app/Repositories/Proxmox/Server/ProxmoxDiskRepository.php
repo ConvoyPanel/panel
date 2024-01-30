486d019e9a9170ea6204fa435bbf24fee648f0b0
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2024-01-30 10:17:37 -0600

    Potential fix for disk resize timeout?

--- app/Repositories/Proxmox/Server/ProxmoxDiskRepository.php
+++ app/Repositories/Proxmox/Server/ProxmoxDiskRepository.php
@@ -2,10 +2,10 @@
 
 namespace Convoy\Repositories\Proxmox\Server;
 
-use Convoy\Models\Server;
-use Webmozart\Assert\Assert;
 use Convoy\Enums\Server\DiskInterface;
+use Convoy\Models\Server;
 use Convoy\Repositories\Proxmox\ProxmoxRepository;
+use Webmozart\Assert\Assert;
 
 class ProxmoxDiskRepository extends ProxmoxRepository
 {
@@ -13,18 +13,18 @@ class ProxmoxDiskRepository extends ProxmoxRepository
     {
         Assert::isInstanceOf($this->server, Server::class);
 
-        $gigabytes = $bytes / 1073741824;
+        $kibibytes = floor($bytes / 1024);
 
         $response = $this->getHttpClient()
-            ->withUrlParameters([
-                'node' => $this->node->cluster,
-                'server' => $this->server->vmid,
-            ])
-            ->put('/api2/json/nodes/{node}/qemu/{server}/resize', [
-                'disk' => $disk->value,
-                'size' => "+{$gigabytes}G",
-            ])
-            ->json();
+                         ->withUrlParameters([
+                             'node' => $this->node->cluster,
+                             'server' => $this->server->vmid,
+                         ])
+                         ->put('/api2/json/nodes/{node}/qemu/{server}/resize', [
+                             'disk' => $disk->value,
+                             'size' => "{$kibibytes}K",
+                         ])
+                         ->json();
 
         return $this->getData($response);
     }
--- app/Services/Servers/SyncBuildService.php
+++ app/Services/Servers/SyncBuildService.php
@@ -2,25 +2,24 @@
 
 namespace Convoy\Services\Servers;
 
-use Convoy\Models\Server;
-use Illuminate\Support\Arr;
-use Convoy\Enums\Server\DiskInterface;
 use Convoy\Data\Server\Proxmox\Config\DiskData;
-use Convoy\Repositories\Proxmox\Server\ProxmoxDiskRepository;
-use Convoy\Repositories\Proxmox\Server\ProxmoxPowerRepository;
+use Convoy\Enums\Server\DiskInterface;
+use Convoy\Models\Server;
 use Convoy\Repositories\Proxmox\Server\ProxmoxConfigRepository;
+use Convoy\Repositories\Proxmox\Server\ProxmoxDiskRepository;
+use Illuminate\Support\Arr;
 
 class SyncBuildService
 {
     public function __construct(
-        private AllocationService $allocationService,
-        private CloudinitService $cloudinitService,
-        private NetworkService $networkService,
-        private ServerDetailService $detailService,
-        private ProxmoxPowerRepository $powerRepository,
+        private AllocationService       $allocationService,
+        private CloudinitService        $cloudinitService,
+        private NetworkService          $networkService,
+        private ServerDetailService     $detailService,
         private ProxmoxConfigRepository $allocationRepository,
-        private ProxmoxDiskRepository $diskRepository,
-    ) {
+        private ProxmoxDiskRepository   $diskRepository,
+    )
+    {
     }
 
     public function handle(Server $server)
@@ -41,16 +40,23 @@ class SyncBuildService
 
         // find a disk that has a corresponding disk in the deployment
         $disksArray = collect($disks->toArray())->pluck('interface')->all();
-        $bootOrder = array_filter(collect($bootOrder->filter(fn (DiskData $disk) => ! $disk->is_media)->toArray())->pluck('interface')->toArray(), fn ($disk) => in_array($disk, $disksArray));
+        $bootOrder = array_filter(
+            collect($bootOrder->filter(fn (DiskData $disk) => !$disk->is_media)->toArray())->pluck(
+                'interface',
+            )->toArray(), fn ($disk) => in_array($disk, $disksArray),
+        );
 
         if (count($bootOrder) > 0) {
             /** @var DiskData $disk */
-            $disk = $disks->where('interface', '=', DiskInterface::from(Arr::first($bootOrder)))->first();
+            $disk = $disks->where('interface', '=', DiskInterface::from(Arr::first($bootOrder)))
+                          ->first();
 
             $diff = $server->disk - $disk->size;
 
             if ($diff > 0) {
-                $this->diskRepository->setServer($server)->resizeDisk($disk->interface, $diff);
+                $this->diskRepository->setServer($server)->resizeDisk(
+                    $disk->interface, $server->disk,
+                );
             }
         }
     }
