d544501c1284c716a72b7edcc8a838cc51555f00
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2024-02-16 09:41:05 -0600

    Delete dead AddressController routes

--- resources/scripts/components/admin/nodes/addresses/DeleteAddressModal.tsx
+++ resources/scripts/components/admin/nodes/addresses/DeleteAddressModal.tsx
@@ -2,7 +2,7 @@ import { useFlashKey } from '@/util/useFlash'
 import usePagination from '@/util/usePagination'
 import { FormikProvider, useFormik } from 'formik'
 
-import deleteAddress from '@/api/admin/nodes/addresses/deleteAddress'
+import deleteAddress from '@/api/admin/addressPools/addresses/deleteAddress'
 import useAddressesSWR from '@/api/admin/nodes/addresses/useAddressesSWR'
 import useNodeSWR from '@/api/admin/nodes/useNodeSWR'
 import { Address } from '@/api/server/getServer'
@@ -17,23 +17,23 @@ interface Props {
     address: Address
 }
 
-const DeleteAddressModal = ({ open, onClose, address }: Props) => {
-    const { data: node } = useNodeSWR()
-    const { clearFlashes, clearAndAddHttpError } = useFlashKey(
-        `admin.nodes.${node.id}.addresses.${address.id}.delete`
+const DeleteAddressModal = ({open, onClose, address}: Props) => {
+    const {data: node} = useNodeSWR()
+    const {clearFlashes, clearAndAddHttpError} = useFlashKey(
+            `admin.nodes.${node.id}.addresses.${address.id}.delete`
     )
     const [page] = usePagination()
-    const { mutate } = useAddressesSWR(node.id, { page, include: ['server'] })
+    const {mutate} = useAddressesSWR(node.id, {page, include: ['server']})
 
     const form = useFormik({
         initialValues: {
             syncNetworkConfig: true,
         },
-        onSubmit: async ({ syncNetworkConfig }, { setSubmitting }) => {
+        onSubmit: async ({syncNetworkConfig}, {setSubmitting}) => {
             clearFlashes()
             setSubmitting(true)
             try {
-                await deleteAddress(node.id, address.id, syncNetworkConfig)
+                await deleteAddress(address.addressPoolId, address.id)
 
                 mutate(data => {
                     if (!data) return data
@@ -52,34 +52,34 @@ const DeleteAddressModal = ({ open, onClose, address }: Props) => {
     })
 
     return (
-        <Modal open={open} onClose={onClose}>
-            <Modal.Header>
-                <Modal.Title>Delete Address</Modal.Title>
-            </Modal.Header>
-            <FormikProvider value={form}>
-                <form onSubmit={form.handleSubmit}>
-                    <Modal.Body>
-                        <FlashMessageRender
-                            className='mb-5'
-                            byKey={`admin.nodes.${node.id}.addresses.${address.id}.delete`}
-                        />
+            <Modal open={open} onClose={onClose}>
+                <Modal.Header>
+                    <Modal.Title>Delete Address</Modal.Title>
+                </Modal.Header>
+                <FormikProvider value={form}>
+                    <form onSubmit={form.handleSubmit}>
+                        <Modal.Body>
+                            <FlashMessageRender
+                                    className='mb-5'
+                                    byKey={`admin.nodes.${node.id}.addresses.${address.id}.delete`}
+                            />
 
-                        <Modal.Description>
-                            Are you sure you want to delete this address? This
-                            action cannot be undone.
-                        </Modal.Description>
-                    </Modal.Body>
-                    <Modal.Actions>
-                        <Modal.Action type='button' onClick={onClose}>
-                            Cancel
-                        </Modal.Action>
-                        <Modal.Action type='submit' loading={form.isSubmitting}>
-                            Delete
-                        </Modal.Action>
-                    </Modal.Actions>
-                </form>
-            </FormikProvider>
-        </Modal>
+                            <Modal.Description>
+                                Are you sure you want to delete this address? This
+                                action cannot be undone.
+                            </Modal.Description>
+                        </Modal.Body>
+                        <Modal.Actions>
+                            <Modal.Action type='button' onClick={onClose}>
+                                Cancel
+                            </Modal.Action>
+                            <Modal.Action type='submit' loading={form.isSubmitting}>
+                                Delete
+                            </Modal.Action>
+                        </Modal.Actions>
+                    </form>
+                </FormikProvider>
+            </Modal>
     )
 }
 
