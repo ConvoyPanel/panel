620e4b4c6ae07a82d68f56bb5dadc6ef45c312c2
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2024-03-04 14:29:05 -0600

    Format ServerControllerTest.php

--- tests/Feature/Controllers/Client/Servers/ServerControllerTest.php
+++ tests/Feature/Controllers/Client/Servers/ServerControllerTest.php
@@ -1,20 +1,34 @@
 <?php
 
-beforeEach(fn () => Http::preventStrayRequests());
-
 it('can generate noVNC authorization token', function () {
     Http::fake([
-        '*/api2/json/access/users' => Http::response(file_get_contents(base_path('tests/Fixtures/Repositories/Node/CreateUserData.json')), 200),
-        '*/api2/json/access/roles' => Http::response(file_get_contents(base_path('tests/Fixtures/Repositories/Node/CreateRoleData.json')), 200),
-        '*/api2/json/access/acl' => Http::response(file_get_contents(base_path('tests/Fixtures/Repositories/Server/AddUserToServerData.json')), 200),
-        '*/api2/json/access/ticket' => Http::response(file_get_contents(base_path('tests/Fixtures/Repositories/Node/CreateUserTicketData.json')), 200),
+        '*/api2/json/access/users' => Http::response(
+            file_get_contents(base_path('tests/Fixtures/Repositories/Node/CreateUserData.json')),
+            200,
+        ),
+        '*/api2/json/access/roles' => Http::response(
+            file_get_contents(base_path('tests/Fixtures/Repositories/Node/CreateRoleData.json')),
+            200,
+        ),
+        '*/api2/json/access/acl' => Http::response(
+            file_get_contents(
+                base_path('tests/Fixtures/Repositories/Server/AddUserToServerData.json'),
+            ), 200,
+        ),
+        '*/api2/json/access/ticket' => Http::response(
+            file_get_contents(
+                base_path('tests/Fixtures/Repositories/Node/CreateUserTicketData.json'),
+            ), 200,
+        ),
     ]);
 
     [$user, $_, $_, $server] = createServerModel();
 
-    $response = $this->actingAs($user)->postJson("/api/client/servers/{$server->uuid}/create-console-session", [
-        'type' => 'novnc'
-    ]);
+    $response = $this->actingAs($user)->postJson(
+        "/api/client/servers/{$server->uuid}/create-console-session", [
+        'type' => 'novnc',
+    ],
+    );
 
     $response->assertOk();
 });
