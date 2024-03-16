70d01d8c0b0980b5fa65b79971a0d3b04d71eae6
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2024-03-16 16:23:47 -0500

    add login redirect

--- resources/scripts/routes/auth/login.lazy.tsx
+++ resources/scripts/routes/auth/login.lazy.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { IconBrandOauth } from '@tabler/icons-react'
-import { createLazyFileRoute, redirect } from '@tanstack/react-router'
+import { createLazyFileRoute } from '@tanstack/react-router'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
@@ -18,8 +18,13 @@ import { Form, FormButton } from '@/components/ui/Form'
 import { InputForm } from '@/components/ui/Forms'
 
 
+const searchSchema = z.object({
+    redirect: z.string().optional(),
+})
+
 export const Route = createLazyFileRoute('/auth/login')({
     component: Login,
+    validateSearch: searchSchema,
 })
 
 const schema = z.object({
@@ -28,6 +33,7 @@ const schema = z.object({
 })
 
 function Login() {
+    const { redirect } = Route.useSearch()
     const form = useForm<z.infer<typeof schema>>({
         resolver: zodResolver(schema),
         defaultValues: {
@@ -40,51 +46,52 @@ function Login() {
         try {
             await login(data)
 
-            redirect('/')
-        } catch {}
+            redirect(redirect ? `/${redirect.slice(1)}` : '/')
+        } catch {
+        }
     }
 
     return (
-        <>
-            <CardHeader className='space-y-2'>
-                <CardTitle className='text-3xl'>Sign in</CardTitle>
-                <CardDescription>
-                    Enter your credentials to continue
-                </CardDescription>
-            </CardHeader>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)}>
-                    <CardContent className='grid gap-4'>
-                        <InputForm
-                            name={'email'}
-                            label={'Email'}
-                            type={'email'}
-                        />
-                        <InputForm
-                            name={'password'}
-                            label={'Password'}
-                            type={'password'}
-                        />
-                    </CardContent>
-                    <CardFooter className={'flex flex-col space-y-4'}>
-                        <FormButton className='w-full'>Sign in</FormButton>
-                        <div className='relative w-full'>
-                            <div className='absolute inset-0 flex items-center'>
-                                <span className='w-full border-t' />
-                            </div>
-                            <div className='relative flex justify-center text-xs uppercase'>
-                                <span className='bg-background px-2 text-muted-foreground'>
+            <>
+                <CardHeader className="space-y-2">
+                    <CardTitle className="text-3xl">Sign in</CardTitle>
+                    <CardDescription>
+                        Enter your credentials to continue
+                    </CardDescription>
+                </CardHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                        <CardContent className="grid gap-4">
+                            <InputForm
+                                    name={'email'}
+                                    label={'Email'}
+                                    type={'email'}
+                            />
+                            <InputForm
+                                    name={'password'}
+                                    label={'Password'}
+                                    type={'password'}
+                            />
+                        </CardContent>
+                        <CardFooter className={'flex flex-col space-y-4'}>
+                            <FormButton className="w-full">Sign in</FormButton>
+                            <div className="relative w-full">
+                                <div className="absolute inset-0 flex items-center">
+                                    <span className="w-full border-t" />
+                                </div>
+                                <div className="relative flex justify-center text-xs uppercase">
+                                <span className="bg-background px-2 text-muted-foreground">
                                     Or continue with
                                 </span>
+                                </div>
                             </div>
-                        </div>
-                        <Button className={'w-full'} variant='outline'>
-                            <IconBrandOauth className='mr-2 h-4 w-4' />
-                            OIDC Provider
-                        </Button>
-                    </CardFooter>
-                </form>
-            </Form>
-        </>
+                            <Button className={'w-full'} variant="outline">
+                                <IconBrandOauth className="mr-2 h-4 w-4" />
+                                OIDC Provider
+                            </Button>
+                        </CardFooter>
+                    </form>
+                </Form>
+            </>
     )
 }
