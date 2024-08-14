049b6fc3ec105f43825022dfae67a4fc10512a8c
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2024-08-13 21:38:28 -0500

    Make padding consistent

--- resources/scripts/components/layouts/AppLayout.tsx
+++ resources/scripts/components/layouts/AppLayout.tsx
@@ -15,12 +15,10 @@ const AppLayout = ({ routes, children }: Props) => {
             <Sidebar routes={routes} />
             <div className='flex grow flex-col overflow-x-hidden sm:gap-4 sm:py-4'>
                 <Header routes={routes} />
-                <main
-                    className={
-                        '@container flex h-full flex-col gap-4 p-4 sm:px-6 sm:py-0 md:gap-8'
-                    }
-                >
-                    {children}
+                <main className={'h-full  p-4 @container  sm:px-6 sm:py-0'}>
+                    <div className={'flex h-full flex-col gap-2 @md:gap-4'}>
+                        {children}
+                    </div>
                 </main>
             </div>
         </div>
