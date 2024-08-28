d84ed76c240b02af6f106c9b9f3e63d2dc03b9ce
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2024-08-28 12:01:11 -0400

    Fix casing in data object

--- app/Data/Server/Proxmox/Usages/ServerTimepointData.php
+++ app/Data/Server/Proxmox/Usages/ServerTimepointData.php
@@ -8,8 +8,8 @@ use Spatie\LaravelData\Data;
 class ServerTimepointData extends Data
 {
     public function __construct(
-        public int               $cpuUsed,
-        public int               $memoryUsed,
+        public int               $cpu_used,
+        public int               $memory_used,
         public ServerNetworkData $network,
         public ServerDiskData    $disk,
         public CarbonImmutable   $timestamp,
