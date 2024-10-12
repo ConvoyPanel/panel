a19a5d59071d572653d227f870e92fc3a3fa2275
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2024-10-12 16:55:40 -0400

    Add types to ProxmoxConfigRepository.php

--- app/Repositories/Proxmox/Server/ProxmoxConfigRepository.php
+++ app/Repositories/Proxmox/Server/ProxmoxConfigRepository.php
@@ -3,12 +3,12 @@
 namespace Convoy\Repositories\Proxmox\Server;
 
 use Convoy\Models\Server;
-use Webmozart\Assert\Assert;
 use Convoy\Repositories\Proxmox\ProxmoxRepository;
+use Webmozart\Assert\Assert;
 
 class ProxmoxConfigRepository extends ProxmoxRepository
 {
-    public function getConfig()
+    public function getConfig(): array
     {
         Assert::isInstanceOf($this->server, Server::class);
 
