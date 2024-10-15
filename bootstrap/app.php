100c62f4f2cb9dae732b0f38ebe528b036a279fc
Shift <shift@laravelshift.com>
2024-10-15 00:33:12 +0000

    Consolidate service providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Providers\AppServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+        $middleware->redirectUsersTo(AppServiceProvider::HOME);
 
         $middleware->alias([
             'auth' => \App\Http\Middleware\Authenticate::class,
