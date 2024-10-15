ca0a90e77577ea55dede3082f11aa97229184bb7
Shift <shift@laravelshift.com>
2024-10-15 00:33:12 +0000

    Re-register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
+    App\Providers\ActivityLogServiceProvider::class,
     App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\RepositoryServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
 ];
--- config/app.php
+++ config/app.php
@@ -7,24 +7,6 @@ return [
 
     'version' => 'canary',
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\ActivityLogServiceProvider::class,
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\RepositoryServiceProvider::class,
-        App\Providers\FortifyServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         // Custom Facades
