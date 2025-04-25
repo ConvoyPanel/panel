260dbe1f2345309c670401c68067ed71b226bb90
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2025-04-25 18:39:59 -0400

    Refactor SortableStorageCard for improved UI and functionality
    Updated the component's structure and styling to enhance readability and user interaction. Added support for drag handles, badges for storage names, and better handling of descriptions. Improved class management and resolved minor code formatting issues.

--- resources/scripts/components/interfaces/Admin/Node/Storages/SortableStorageCard.tsx
+++ resources/scripts/components/interfaces/Admin/Node/Storages/SortableStorageCard.tsx
@@ -1,7 +1,10 @@
 import { NodeStorage } from '@/types/storage.ts'
+import { cn } from '@/utils'
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
-import { cn } from '@/utils'
+import { IconGripVertical } from '@tabler/icons-react'
+
+import { badgeVariants } from '@/components/ui/Badge.tsx'
 
 interface Props {
     storage: NodeStorage
@@ -9,10 +12,16 @@ interface Props {
 }
 
 const SortableStorageCard = ({ storage, isOverlay = false }: Props) => {
-    const { attributes, listeners, setNodeRef, transform, transition, isDragging } =
-        useSortable({
-            id: storage.id,
-        })
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        transition,
+        isDragging,
+    } = useSortable({
+        id: storage.id,
+    })
 
     const style = {
         transform: CSS.Transform.toString(transform),
@@ -21,13 +30,43 @@ const SortableStorageCard = ({ storage, isOverlay = false }: Props) => {
 
     return (
         <li
-            className={cn('rounded-md py-2 px-4 border bg-card text-card-foreground shadow', isOverlay && 'z-10', isDragging && 'brightness-50 opacity-50')}
+            className={cn(
+                'relative flex flex-col rounded-lg border bg-card py-2 pl-7 pr-2 text-card-foreground shadow select-none',
+                isOverlay && 'z-10',
+                isDragging && 'opacity-50 brightness-50'
+            )}
             ref={setNodeRef}
             style={style}
             {...attributes}
             {...listeners}
         >
-            <p>{storage?.displayName ?? storage.name}</p>
+            <IconGripVertical
+                className={
+                    'absolute left-1 top-1/2 shrink-0 -translate-y-1/2 text-muted-foreground'
+                }
+            />
+            <div className={'flex items-center'}>
+                <p className={'inline truncate font-semibold'}>
+                    {storage?.displayName ?? storage.name} as f df s
+                </p>
+                {storage.displayName && (
+                    <span
+                        className={cn(
+                            'shrink-0F ml-1.5 inline-flex align-middle tracking-tight',
+                            badgeVariants({
+                                variant: 'secondary',
+                            })
+                        )}
+                    >
+                        {storage.name}
+                    </span>
+                )}
+            </div>
+            {storage.description && (
+                <p className={'text-ellipsis text-sm text-muted-foreground'}>
+                    {storage.description}
+                </p>
+            )}
         </li>
     )
 }
