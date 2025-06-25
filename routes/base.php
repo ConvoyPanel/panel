1df9a7b34e910d671cfaf0281533bff608f4e355
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2025-06-24 23:01:49 -0500

    Nuke LocaleController.php

--- routes/base.php
+++ routes/base.php
@@ -6,8 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [Base\IndexController::class, 'index'])->name('index')
      ->fallback();
 
-Route::get('/locales/locale.json', Base\LocaleController::class)
-     ->where('namespace', '.*');
-
 Route::get('/{any}', [Base\IndexController::class, 'index'])
      ->where('any', '^(?!(\/)?(api|authorize)).+');
