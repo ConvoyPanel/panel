58ef0fbd17450770aeb7f1c6323f393cf9ced35b
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2025-07-12 20:37:49 -0500

    Make updateTemplate.ts use UUID

--- resources/scripts/api/admin/templateGroups/templates/updateTemplate.ts
+++ resources/scripts/api/admin/templateGroups/templates/updateTemplate.ts
@@ -4,14 +4,14 @@ import { rawDataToTemplate } from '@/api/transformers/template.ts';
 import { createTemplateSchema } from '@/api/admin/templateGroups/templates/createTemplate.ts';
 
 const updateTemplate = async (
-    templateGroupId: number,
+    templateGroupUuid: string,
     templateId: number,
     { isAdminOnly, ...rest }: z.infer<typeof createTemplateSchema>
 ) => {
     const {
         data: { data },
     } = await axios.put(
-        `/api/admin/template-groups/${templateGroupId}/templates/${templateId}`,
+        `/api/admin/template-groups/${templateGroupUuid}/templates/${templateId}`,
         {
             ...rest,
             is_admin_only: isAdminOnly,
