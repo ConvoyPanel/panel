81d4f4d234dfb8b8005783f1888d1f06c218ca60
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2025-07-21 21:29:56 -0500

    Enhance server case-insensitive search

--- app/Models/Filters/FiltersServerWildcard.php
+++ app/Models/Filters/FiltersServerWildcard.php
@@ -4,15 +4,21 @@ namespace App\Models\Filters;
 
 use Illuminate\Database\Eloquent\Builder;
 use Spatie\QueryBuilder\Filters\Filter;
+use function strtolower;
 
 class FiltersServerWildcard implements Filter
 {
     public function __invoke(Builder $query, $value, string $property): void
     {
-        $query->where('id', $value)
-              ->orWhere('uuid', $value)
-              ->orWhere('uuid_short', $value)
-              ->orWhereRaw('LOWER(hostname) LIKE ?', ["%$value%"])
-              ->orWhereRaw('LOWER(name) LIKE ?', ["%$value%"]);
+        if ($value === '') {
+            return;
+        }
+
+        $query->where(function (Builder $query) use ($value) {
+            $query->whereRaw('LOWER(hostname) LIKE ?', ['%' . strtolower($value) . '%'])
+                  ->orWhereRaw('LOWER(name) LIKE ?', ['%' . strtolower($value) . '%'])
+                  ->orWhereRaw('LOWER(uuid) LIKE ?', ['%' . strtolower($value) . '%'])
+                  ->orWhereRaw('LOWER(uuid_short) LIKE ?', ['%' . strtolower($value) . '%']);
+        });
     }
 }
