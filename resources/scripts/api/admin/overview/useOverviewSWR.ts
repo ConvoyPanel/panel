9f8eec25d2967e4d40fa4100ca8c2a321e1ef9c1
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2026-05-28 21:27:58 -0400

    refactor: remove unnecessary SWR revalidation config

--- resources/scripts/api/admin/overview/useOverviewSWR.ts
+++ resources/scripts/api/admin/overview/useOverviewSWR.ts
@@ -3,10 +3,7 @@ import useSWR from 'swr'
 import getOverview, { DashboardOverview } from '@/api/admin/overview/getOverview'
 
 const useOverviewSWR = () => {
-    return useSWR<DashboardOverview>('admin:overview', getOverview, {
-        refreshInterval: 10000,
-        revalidateOnFocus: true,
-    })
+    return useSWR<DashboardOverview>('admin:overview', getOverview)
 }
 
 export default useOverviewSWR
